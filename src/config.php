fc511c167fd293e128c2aed88a71a0b33d4f4e57
root <root@hazemhagrass.(none)>
2014-03-12 16:38:09 -0400

    update offline_access_tokens

--- src/config.php
+++ src/config.php
@@ -4,7 +4,8 @@ $config = array(
 	'app_secret' => '59d334831df66a763c29e94197350122',
 	'page_id' => '340235596078780',
 	'app_access_token' => '238816542959619|U5cb6Fx9rM4cEg8vOJL3rDXIPWk',
-	'offline_access_token' => 'CAADZAM881qAMBAPosCTNzxKHYRDByJfsFmmMevoFYI7ZBpXOvNDZAYGkRN1hk5eKgawXPNkLNG7W24SYfy1x5qLkZCzObTHJXnLtgbYj81EPf6R7bOCuzanET2sL6AU8ITfFePqOFpbhzhqWWfNZCnZCZAeevxVaAPbE1b468CSBZB8jbXPgdxPvY9H0GvjsyTUZD',
+	'offline_access_token' => 'CAADZAM881qAMBAF2xbrzVozhz1HFXk2uO7r0R8hjkAzecd6TsJMoZA4TjeFVTOiO2V4cZCzkf3GFZB9F1oFYTOb5olCj41bpfkzzJ4YonFGrh0E5SwKNZBBkAOWo3TbEUkfZAReZCvRTTuQhYJLZCIGAHXy76r9ORrf1T1ZAu2xZAoU1730Uw3hZBrkZAWdmEVGPvmAZD',
+	#'offline_access_token' => 'CAADZAM881qAMBAPosCTNzxKHYRDByJfsFmmMevoFYI7ZBpXOvNDZAYGkRN1hk5eKgawXPNkLNG7W24SYfy1x5qLkZCzObTHJXnLtgbYj81EPf6R7bOCuzanET2sL6AU8ITfFePqOFpbhzhqWWfNZCnZCZAeevxVaAPbE1b468CSBZB8jbXPgdxPvY9H0GvjsyTUZD',
 	'return_url' => 'http://hazemhagrass.com:8080/',
 	'code' => 'AQBvB2yRbqIiRxe0UfSa8vjE3mMpWx4iTuTHiRbUJ0autZNjyR2CiGKFvSdxBPPS4CHo2nV-ciO_z71N8Ez1kfa-Ppf-qhGIaSGIOUcFhJnwtBGf2gn8s8w8rLGA_lJ8CYkgXQituyuMCvZ14Py5r7ZIvw-Attk2c85oI_wkASk3nr_ZEXZxD87tT_VAE6nqRLLwBUEu3pcTy-ZiYLrHH075W1xvdILzeT1aqMeNsPj3gwM7HSxP3IXBwoE3QNau8bPbuMFC9AyY-j2uSQcVGaFfEnRVHbuM6eI4_G0YXhCQOSgktkbaSgXutgketKCro-o#_=_',
 	'permissions' => 'manage_pages,offline_access,publish_stream'
